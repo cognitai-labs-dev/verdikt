383cd8e6860b440126a51f53b782f5ce8eba539d
SamoKopecky <sano.kopecky@gmail.com>
2026-02-08 21:33:27 +0100

    Revert "feat: Remove hash"
    This reverts commit f7bed9d41c0ebe73289cb3b028377293feee8514.

--- src/schemas/prompt_version.py
+++ src/schemas/prompt_version.py
@@ -1,18 +1,34 @@
+import hashlib
 from datetime import datetime
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 
 class PromptVersionCreateSchema(BaseModel):
     app_id: int = Field(description="App this prompt belongs to")
+    hash: str | None = Field(
+        default=None,
+        max_length=64,
+        description="SHA-256 hash of the content, don't provide gets calculated",
+    )
     content: str = Field(
         description="Prompt content",
     )
+
+    @model_validator(mode="after")
+    def generate_hash(self) -> "PromptVersionCreateSchema":
+        content_bytes = self.content.encode("utf-8")
+        self.hash = hashlib.sha256(content_bytes).hexdigest()
+        return self
 
 
 class PromptVersionSchema(BaseModel):
     id: int = Field(description="Unique identifier")
     app_id: int = Field(description="App this prompt belongs to")
+    hash: str = Field(
+        max_length=64,
+        description="SHA-256 hash of the content",
+    )
     content: str = Field(
         description="Prompt content",
     )
